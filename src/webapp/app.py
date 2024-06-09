a14f2bfec0e22c931211fe39f78378d8f171e26c
adriano22_ <califano993@gmail.com>
2024-06-09 18:38:56 +0200

    add g6 test

--- src/webapp/app.py
+++ src/webapp/app.py
@@ -1,9 +1,7 @@
 import utilities, flask, json
 from flask_cors import cross_origin
-from flask_visjs import VisJS4, Network
 
 app = flask.Flask(__name__, template_folder = "templateFiles", static_folder = "staticFiles")
-VisJS4().init_app(app)
 
 @app.route("/meshterms_graph", methods = ["GET", "POST"])
 @cross_origin()
@@ -21,21 +19,19 @@ def meshterms_graph():
     return json.dumps({"graph_nodes": graph_nodes, "graph_edges": graph_edges})
 
 @app.route("/network", methods = ["GET", "POST"])
+@cross_origin()
 def network():
     graph = utilities.extract_graph(filename = "data/graph_ner.pkl")
-
-    network = Network("1500px", "1000px", directed = False)
-    network.add_nodes(graph.nodes)
-
-    count = 0
-
+    
+    graph_nodes = []
+    for node in graph.nodes:
+        graph_nodes.append({"id": str(node), "label": str(node)})
+        
+    graph_edges = []
     for edge in graph.edges(data = True):
-        if(count < 100):
-            network.add_edge(int(edge[0]), int(edge[1]), title = edge[2]["weight"])
-            print("Aggiunto l'arco: ", edge)
-            count += 1
+        graph_edges.append({"source": str(edge[0]), "target": str(edge[1]), "label": str(edge[2]["weight"])})
 
-    return flask.render_template("ner_index.html", network = network)
+    return json.dumps({"graph_nodes": graph_nodes, "graph_edges": graph_edges})
 
 if __name__ == "__main__":
     app.run(port = 5000, debug=True)
