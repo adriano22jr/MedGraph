7613b20a09977e6211b86b718dff6936b4697221
Anthony Zunico <91367092+DJHeisenberg01@users.noreply.github.com>
2024-06-27 13:03:00 +0200

    clean folder 2

--- src/webapp/my-react-app/src/index.js
+++
@@ -1,8 +0,0 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
-import $ from 'jquery';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-
-ReactDOM.render(<App />, document.getElementById('root'));
--- src/webapp/tempCodeRunnerFile.py
+++
@@ -1,1 +0,0 @@
-        print(graph.nodes(data = True))
